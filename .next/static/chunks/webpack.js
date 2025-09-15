c47efafe23167f018e5b080793a5ebdad94380b4
ralphestephan <ralph.estephan@icloud.com>
2025-09-15 22:51:58 +0300

    product view

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "73c99886cac6dd8a"; }
+/******/ 		__webpack_require__.h = function() { return "5816dc1b59ef3986"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- app/api/debug/orders/route.ts
+++
@@ -1,8 +0,0 @@
-// app/api/debug/orders/route.ts
-export const runtime = 'nodejs';
-import { NextResponse } from 'next/server';
-import { prisma } from '@/lib/db';
-export async function GET() {
-  const count = await prisma.order.count();
-  return NextResponse.json({ ok: true, count });
-}
